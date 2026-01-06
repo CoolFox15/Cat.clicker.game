4251a2d98b30cbced50048dd1953db1093985e79
CoolFox15 <glemmestdella1506@gmail.com>
2026-01-06 14:16:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
     let count = 0;
+    let cps = 0;
+    let autoCost = 150;
+    let priceMultiplier = 1.5; 
+    
     const button = document.getElementById("clickBtn");
     const display = document.getElementById("count");
     const reset = document.getElementById("resetBtn");
+    const autoBtn = document.getElementById("autoClickBtn");
+    const autoCostSpan = document.getElementById("autocost";
 
     button.addEventListener("click", function() {
         count++;
@@ -14,4 +20,22 @@ document.addEventListener("DOMContentLoaded", function() {
     count = 0;                
     display.textContent = count; 
     });
+
+    autoCostSpan.innerText = autoCost; 
+    autoBtn.addEventListener("click", () => {
+        if (clicks >= autoCost) {
+            clicks -= autoCost;
+            cps += 1;
+
+            autoCost = math.floor(autoCOst * priceMultiplier);
+            autoCostSpan.innerText = autoCost;
+        } else {
+            alert("Not enough clicks!");
+        }
+    });
+
+    SetInterval(() => {
+        clicks += cps;
+    }, 1000);
+    
 });
